13faaacfe80a15fea627fe1eaf3a27d55aef106b
Erik Björn <erik.g.bjoern@gmail.com>
2020-04-15 15:54:48 +0200

    Installing React Router (BrowserRouter) and creating new headings About and Projects to navigate to using Route. Testing navigation

--- src/index.js
+++ src/index.js
@@ -3,15 +3,26 @@ import ReactDOM from "react-dom"
 import Header from "./Header"
 import Hello from "./Hello"
 import Footer from "./Footer"
+import About from "./About"
+import Projects from "./Projects"
+import { Switch, Route, BrowserRouter } from "react-router-dom"
 
 const App = () => {
     return (
-        <div>
+        <>
             <Header />
-            <Hello />
+            <Switch />
+                <Route exact path="/" component={Hello}></Route>
+                <Route exact path="/about" component={About}></Route>
+                <Route exact path="/projects" component={Projects}></Route>
             <Footer />
-        </div>
+        </>
     )
 }
 
-ReactDOM.render(<App />, document.getElementById("app"))
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById("app")
+)
