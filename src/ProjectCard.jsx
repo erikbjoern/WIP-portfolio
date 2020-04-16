40f9dbd5d17c87c3915daa250a8c5c1eba9c40ff
Erik Björn <erik.g.bjoern@gmail.com>
2020-04-16 10:46:43 +0200

    Refactoring projectsList using new module ProjectCard as a sort of template for projects. Still renders perfectly.

---
+++ src/ProjectCard.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+const ProjectCard = ({project}) => {
+    return (
+        <>
+            <div className="ui card">
+                <div className="image">
+                    <img src={project.image} />
+                </div>
+                <div className="content">
+                    <h3 className="ui header">{project.name}</h3>
+                    <div className="description">{project.description}</div>
+                </div>
+            </div>
+        </>    
+    )
+}
+
+export default ProjectCard
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import axios from "axios"
+import ProjectCard from "./ProjectCard"
 
 class Projects extends Component { 
     state = {
@@ -15,9 +16,7 @@ class Projects extends Component {
             projectsList = projects.map(project => {
                 return ( 
                     <div id={`project-${project.id}`} key={project.id}>
-                        <h3 className="ui header">{project.name}</h3>
-                        <img className="image" src={project.image} alt="image"/>
-                        <p className="description">{project.description}</p>
+                        <ProjectCard project={project} />
                     </div>
                 )
             })
@@ -26,7 +25,7 @@ class Projects extends Component {
         return (
             <div className="ui main container">
                 <h1 id="projects-header" className="ui header">My Projects</h1>
-                {projectsList}
+                <div className="ui stackable four column grid">{projectsList}</div>
             </div>
         )
     }
