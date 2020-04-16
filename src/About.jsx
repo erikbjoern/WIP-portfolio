cd79f3c5ce0fa0019e667121d46fa6fb1242dad8
Erik Björn <erik.g.bjoern@gmail.com>
2020-04-16 11:28:06 +0200

    adding illustrations to About and Projects tabs, with react-undraw-illustrations

--- src/About.jsx
+++ src/About.jsx
@@ -1,14 +1,22 @@
 import React from "react"
+import { UndrawByMyCar } from "react-undraw-illustrations"
 
 const About = () => {
     return (
         <div className="ui main container">
-            <h1 id="about-header" className="ui header">About Me</h1>
-            <p>
-                Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident
-                corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum
-                molestias?
-            </p>
+            <div className="ui stackable two column grid">
+                <div className="column">
+                    <UndrawByMyCar primaryColor='#12283a' height='200px'/>
+                </div>
+                <div className="column">
+                    <h1 id="about-header" className="ui header">About Me</h1>
+                    <p>
+                        Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident
+                        corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum
+                        molestias?
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
