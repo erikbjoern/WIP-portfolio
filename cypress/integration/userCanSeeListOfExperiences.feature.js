82fc80d88860b3987b4177d56bf31190a98beb64
Erik Björn <erik.g.bjoern@gmail.com>
2020-04-19 19:24:12 +0200

    dividing CV into Education and Work experience, adding another education item.

--- cypress/integration/userCanSeeListOfExperiences.feature.js
+++ cypress/integration/userCanSeeListOfExperiences.feature.js
@@ -2,6 +2,16 @@ describe('User can see a list of education and work experience', () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000/cv')
     })
+
+    it('displays heading "Work experience"', () => {
+        cy.get('#work-header').should('contain', 'Work experience')
+    })
+
+    it('displays heading "Education"', () => {
+        cy.get('#education-header').should('contain', 'Education')
+    })
+
+
     describe('displays first item, containing', () => {
         it('header', () => {
             cy.get('#cvItem-1').within(() => {
@@ -23,7 +33,7 @@ describe('User can see a list of education and work experience', () => {
     describe('displays second item, containing', () => {
         it('header', () => {
             cy.get('#cvItem-2').within(() => {
-                cy.get('.ui.header').should('contain', 'Mark & Grund AB')
+                cy.get('.ui.header').should('contain', 'Technology programme')
             })
         })
         it('image', () => {
@@ -33,24 +43,42 @@ describe('User can see a list of education and work experience', () => {
         })
         it('description', () => {
             cy.get('#cvItem-2').within(() => {
+                cy.get('.description').should('contain', 'With specialisation in design and product development.')
+            })
+        })
+    })
+    
+    describe('displays third item, containing', () => {
+        it('header', () => {
+            cy.get('#cvItem-3').within(() => {
+                cy.get('.ui.header').should('contain', 'Mark & Grund AB')
+            })
+        })
+        it('image', () => {
+            cy.get('#cvItem-3').within(() => {
+                cy.get('.image').should('exist')
+            })
+        })
+        it('description', () => {
+            cy.get('#cvItem-3').within(() => {
                 cy.get('.description').should('contain', 'Apprentice carpenter at a groundworks company.')
             })
         })
     })
 
-    describe('displays third item, containing', () => {
+    describe('displays fourth item, containing', () => {
         it('header', () => {
-            cy.get('#cvItem-3').within(() => {
+            cy.get('#cvItem-4').within(() => {
                 cy.get('.ui.header').should('contain', 'Umeå municipality')
             })
         })
         it('image', () => {
-            cy.get('#cvItem-3').within(() => {
+            cy.get('#cvItem-4').within(() => {
                 cy.get('.image').should('exist')
             })
         })
         it('description', () => {
-            cy.get('#cvItem-3').within(() => {
+            cy.get('#cvItem-4').within(() => {
                 cy.get('.description').should('contain', 'Home care service.')
             })
         })
--- src/Cv.jsx
+++ src/Cv.jsx
@@ -9,11 +9,19 @@ class CV extends Component {
 
     render() {
         const cvItems = this.state.cvItems
-        let cvItemsList
+        let workItemsList
+        let educationItemsList
 
         if (cvItems.length > 0) {
-
-            cvItemsList = cvItems.map(cvItem => {
+            
+            educationItemsList = cvItems.slice(0,2).map(cvItem => {
+                return(
+                    <div className="ui card" id={`cvItem-${cvItem.id}`} key={cvItem.id}>
+                        <CvCard cvItem={cvItem} />
+                    </div>
+                )
+            })
+            workItemsList = cvItems.slice(2,4).map(cvItem => {
                 return(
                     <div className="ui card" id={`cvItem-${cvItem.id}`} key={cvItem.id}>
                         <CvCard cvItem={cvItem} />
@@ -27,7 +35,21 @@ class CV extends Component {
                     <div id="cv-header" className="ui header" style={{fontSize: "400%", padding: "50px"}}>
                         CV
                     </div>
-                    <div className="ui stackable four column grid cards">{cvItemsList}</div>    
+                    <div className="ui stackable grid">
+                        <div className="seven wide column">
+                            <div id="education-header" className="ui header" style={{fontSize: "200%", padding: "50px"}}>
+                                Education
+                            </div>
+                            <div className="ui stackable two column grid cards">{educationItemsList}</div>
+                        </div>
+                        <div className="two wide column"></div>
+                        <div className="seven wide column">
+                            <div id="work-header" className="ui header" style={{fontSize: "200%", padding: "50px"}}>
+                                Work experience
+                            </div>
+                            <div className="ui stackable two column grid cards">{workItemsList}</div>
+                        </div>    
+                    </div>
                 </div>
         )
     }
