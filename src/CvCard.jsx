6d8256f4feb2f063ad3a07a5ab678dee6094bdd8
Erik Björn <erik.g.bjoern@gmail.com>
2020-04-20 16:10:32 +0200

    centering images of cards so that they look better when they scale wider, adding height: max-content to body to get rid of scrollbar where it's not needed

--- src/CvCard.jsx
+++ src/CvCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CvCard = ({cvItem}) => {
     return (
         <div className="ui segment" style={{height: '100%'}}>
-            <div style={{height: '150px', display: 'flex', alignItems: 'center'}}>
+            <div style={{height: '150px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
                 <div className="ui medium image">
                         <a href={cvItem.link}>
                             <img id="image-link" src={cvItem.image}></img>
